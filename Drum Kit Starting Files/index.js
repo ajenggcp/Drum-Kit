7f2662e2c6c395d98e76297448ad24b28cc96e4d
ajenggcp <79824040+ajenggcp@users.noreply.github.com>
2021-03-23 02:40:06 +0800

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,78 @@
+//Button Press Listener
+
+var numberOfButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfButton ; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener ("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        sounds(buttonInnerHTML);
+        
+        buttonAnimation(buttonInnerHTML);
+        
+    });
+
+}
+
+//Button Keypress Listener
+
+document.addEventListener("keydown" , function(event) {
+    sounds(event.key);
+    buttonAnimation(event.key);
+
+});
+
+//sounds of the key
+
+function sounds(key){
+    switch (key) {
+            case "w":
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+
+            case "a":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+
+            case "d":
+                var kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+
+            case "j":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+
+            case "k":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+
+            case "l":
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+
+            default: console.log(buttonInnerHTML);
+    }
+}
+
+//button animation
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100);
+}
